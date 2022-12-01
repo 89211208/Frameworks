a6f4b5f2cdbbc0c41813d86a939f666d282050be
Jost Gartner <jost.gartner@gmail.com>
2022-12-01 12:41:50 +0100

    tutorial 1

---
+++ CMS/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+const port = 5000
+
+app.get("/",(req,res)=>{
+res.send("hola")
+})
+
+///App listening on port
+app.listen(process.env.PORT || port, ()=>{
+console.log(`Server is running on port: ${process.env.PORT || port}`)
+})
+
+//Import opur custom modules-controllers
+const novice= require("./routes/novice")
+//Routes
+app.use('/novice', novice);
