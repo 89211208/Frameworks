cb0e956039c4081e33775954ed3bd4165dbe6cc9
Luka Radošević <89211049@student.upr.si>
2022-12-02 16:30:51 +0100

    Trying again

--- Server/SimpleServer.js
+++ Server/SimpleServer.js
@@ -1,6 +1,7 @@
 var https = require("http")
 
 https.createServer((req, res)=>{
-    res.write("That line was created from the server")
+    res.write("That line was created from the server TEST")
+    res.write("\n User is in: " + req.url)
     res.end()
 }).listen(8008)
