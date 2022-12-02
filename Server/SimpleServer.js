2ac275f2675ffe36bafb69155dbf9d7d1aec2bc8
Dokysmoky <89201086@student.upr.si>
2022-12-02 09:37:25 +0100

    pls

---
+++ Server/SimpleServer.js
@@ -0,0 +1,7 @@
+var http=require('http');
+http.createServer((req,res)=>{
+    res.write("<h1>That line has been instantiated from the server...</h2>")
+    res.write("\nUser is in:"+req.url)
+    res.end()
+    }).listen(8000)
+    
