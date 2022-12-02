979c02ae4cd1783b940651634a50bca33db69164
Petar <pvancev@gmail.com>
2022-12-02 16:36:36 +0100

    Lab 1 finished

--- servergit/simpleserver.js
+++ servergit/simpleserver.js
@@ -2,7 +2,8 @@ var http = require("http");
 
 http
   .createServer((req, res) => {
-    res.write("This line was instantiated from the server");
+    res.write("<h1>Gjore</h1>");
+    res.write("\n User is in: " + req.url);
     res.end();
   })
-  .listen(8000);
+  .listen(8050);
