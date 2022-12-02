4e0e5dc20cf32c9fc48c84bc77b127bf1818d83e
Anja Pijak <89211024@student.upr.si>
2022-12-02 16:33:17 +0100

    files ignore

---
+++ CMS/index.js
@@ -0,0 +1,12 @@
+var express =require('express')
+const app = express()
+
+const port = 5009
+
+app.get("/", (req,res)=>{
+    res.send("haiiii")
+})
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log(`Server is running on port: ${process.env.PORT || port}`)
+})
