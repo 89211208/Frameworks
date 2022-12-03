1a3f55f15610cfd6504065c5f32dd7a518d21786
Dugi8 <mirkodugi888@gmail.com>
2022-12-03 11:54:34 +0100

    Tutorial NodeJS finished

---
+++ CMS/index.js
@@ -0,0 +1,14 @@
+const { application } = require("express")
+
+const express = require(express)
+const exp = express()
+
+const port = 5012
+exp.get("/",(req,res)=>{
+    res.send("Thank you..")
+})
+
+// App should listen 
+application.listen(process.env.PORT || port, ()=>{
+    console.log('Server is running on port: ${process.env.PORT || port}')
+})
