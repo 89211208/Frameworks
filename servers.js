a34676519ba935fa2ca402ab8ca6dcd07614cb12
elenanenadovic <elenanenadovic@gmail.com>
2022-12-03 22:21:16 +0100

    adding writeHead

--- servers.js
+++ servers.js
@@ -1,5 +1,6 @@
 var http =require('http'); // read buil-in module
 http.createServer((req,res)=>{
-    res.write("This line has been instantiated from the eli server...")
+    res.writeHead(200,{"Content-Type": "text/html"})
+    res.write("<h1>This line has been instantiated from the server...</h1>")
     res.end()
     }).listen(8000)
