614d9cac23b6b10ece17cb34e10e5b8c301ea3f2
elenanenadovic <elenanenadovic@gmail.com>
2022-12-03 22:22:12 +0100

    adding url code

--- servers.js
+++ servers.js
@@ -2,5 +2,6 @@ var http =require('http'); // read buil-in module
 http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type": "text/html"})
     res.write("<h1>This line has been instantiated from the server...</h1>")
+    res.write("\nUser is in: " + req.url)
     res.end()
     }).listen(8000)
