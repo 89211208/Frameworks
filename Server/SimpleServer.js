bc638014f8f6d440d74a41815acb131a67b343cb
Amar <amar.ustavdija@gmail.com>
2022-12-06 18:35:03 +0100

    Simple server running

---
+++ Server/SimpleServer.js
@@ -0,0 +1,15 @@
+var http = require("http")
+
+http.createServer((req, res)=>{
+	res.write("<h1>This line has been instantiated from the server!</h1>")
+	res.write("\n User is in: " + req.url)
+	res.end()
+}).listen(1337)
+
+
+
+
+
+
+
+
