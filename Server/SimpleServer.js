5bb00ab77095ae776c4c79a0ef8cd2a4690d4768
mircheskib <89211288@student.upr.si>
2022-12-07 17:31:47 +0100

    Added file SampleServer

---
+++ Server/SimpleServer.js
@@ -0,0 +1,7 @@
+var http = require('http')
+
+http.createServer((req, res)=>{
+    res.write("<h1>This line has been instantiated from the server...</h1>")
+    res.write("\n User  is in: " + req.url)
+    res.end()
+}).listen(5002)
