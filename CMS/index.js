83ee84d79e9f4c19c3d565e6eaececacbf59f36d
mircheskib <89211288@student.upr.si>
2022-12-07 16:01:37 +0100

    I finished this session by creating file user, novice, env and dbConn

---
+++ CMS/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+
+//const a = require("dotenv")
+//a.config
+
+require("dotenv").config
+const port = 5002
+
+// Routes 
+const novice = require("./routes/novice")
+
+app.get("/", (req,res)=>{
+    res.send("hola")
+})
+
+app.use("/novice", novice)
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log(`Server is running on port: ${process.env.PORT || port}`)
+})
