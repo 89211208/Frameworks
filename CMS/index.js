76927d80b7899a20d4e8fce6beb4cf051f61863f
RokCiber <rokciber@gmail.com>
2022-12-08 10:43:04 +0100

    2. exercise

---
+++ CMS/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+require("dotenv").config
+const port = 5100
+
+// Routes
+const novice = require("./routes/novice")
+
+app.get("/", (req, res)=>{
+    res.send("Hola muchachos...")
+})
+
+app.use("/novice", novice)
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log(`Server is running on port: ${process.env.PORT 
+        || port}`)
+})
--- Server/SimpleServer.js
+++ Server/SimpleServer.js
@@ -1,7 +1,7 @@
 var http = require('http')
 
 http.createServer((req, res) => {
-    res.write("<h1>This line has been instantiated from the server</h1>")
+    res.write("<h1>This line has been instantiatsdged from the server</h1>")
     res.write("\n User ias in: " + req.url)
     res.end()
-}).listen(8069)
+}).listen(5005)
