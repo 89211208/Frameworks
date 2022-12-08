4a95460ddc2d07f0b017051e5c64f7cca3b2aad3
Luka MP <lukarepen@gmail.com>
2022-12-08 08:55:13 +0100

    lalal

--- Server/SimpleServer.js
+++ Server/SimpleServer.js
@@ -1,6 +1,7 @@
-var http = require('http')
+var http = require("http")
 
 http.createServer((req, res)=>{
-    res.write("This line has been instantiated from the server...")
+    res.write("<h1>This line has been instantiated from the server</h1>")
+    res.write("\n User is in: " + req.url )
     res.end()
-})
+}).listen(8055)
