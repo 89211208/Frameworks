8f25489fe1e6ccd1f3fcf890b871c4b51341cf02
mitkeee <71983548+mitkeee@users.noreply.github.com>
2022-12-09 15:10:28 +0100

    changes

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const express = require('express')
+const app = express()
+require("dotenv").config()
+
+const port = 5062
+
+//Configs
+app.use(express.json())
+
+//Import opur custom modules-controllers
+const novice = require("./routes/novice")
+
+
+
+const conn = require("./DB/dbConn")
+const users = require("./routes/users")
+app.get('/', (req, res)=>{
+    res.send("hello")
+    res.end()
+})
+
+//Routes
+app.use('/novice', novice);
+app.use('/users', users)
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log(`Server is running on port: ${process.env.PORT || port}`)
+})
+
