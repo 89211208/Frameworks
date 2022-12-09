17e5ce44127ff07f295c22cf5f8aa786aeaa645d
Marko Stosic <89211035@student.upr.si>
2022-12-09 08:22:09 +0100

    Commiting changes

---
+++ Server/SimpleServer.js
@@ -0,0 +1,7 @@
+var http = require('http')
+
+http.createServer((req, res)=>{
+    res.write("<h1>This line has been instantiated from server...</h1>")
+    res.write("\nUser is in: "+req.url)
+    res.end()
+}).listen(8007)
