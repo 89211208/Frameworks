2ef3242e91c5a388aa562f3bc1516d78a4d9c9ec
Anastasija02 <89211028@student.upr.si>
2022-12-09 21:40:23 +0100

    Backend 1/3

--- CMS/index.js
+++ CMS/index.js
@@ -1,12 +1,30 @@
-const express = require("express")
-const app = express()
-const port = 5000
 
-app.get("/", (req, res)=>{
-    res.send("Hola muchachos...")
+const express = require('express')
+const app = express()
+require("dotenv").config()
+
+const port = 5053
+
+//Configs
+app.use(express.json())
+
+//Import opur custom modules-controllers
+const novice = require("./routes/novice")
+
+
+
+const conn = require("./DB/dbConn")
+const users = require("./routes/users")
+app.get('/', (req, res)=>{
+    res.send("hello friends")
+    res.end()
 })
+
+//Routes
+app.use('/novice', novice);
+app.use('/users', users)
 
 app.listen(process.env.PORT || port, ()=>{
-    console.log(`Server is running on port: ${process.env.PORT 
-        || port}`)
+    console.log(`Server is running on port: ${process.env.PORT || port}`)
 })
+
---
+++ CMS/routes/novice.js
@@ -0,0 +1,57 @@
+const express = require("express")
+const novice = express.Router()
+
+//import db module 
+const DB = require('../DB/dbConn')
+
+novice.get('/',async (req, res) => {
+    try
+    {
+        let queryResult = await DB.allNovice()
+        res.json(queryResult)
+    }
+    catch (error)
+    {
+        console.log(error)
+        res.sendStatus(500)
+    }
+    })
+    //Gets one new based on the id 
+    novice.get('/:id', async (req, res, next) => {
+        try {
+            var queryResult = await DB.oneNovica(req.params.id)
+            res.json(queryResult)
+        }
+        catch (err) {
+            console.log(err)
+            res.sendStatus(500)
+        }
+    })
+//Inserts one new to the database
+novice.post('/', async (req, res, next) => {
+
+    let title = req.body.title
+    let slug = req.body.slug
+    let text = req.body.text
+
+    var isAcompleteNovica = title && slug && text
+    if (isAcompleteNovica) {
+        try {
+            var queryResult = await DB.creteNovica(title, slug, text)
+            if (queryResult.affectedRows) {
+                console.log("New article added!!")
+            }
+        }
+        catch (err) {
+            console.log(err)
+            res.sendStatus(500)
+        }
+    }
+    else {
+        console.log("A field is empty!!")
+    }
+    res.end()
+
+
+})
+    module.exports = novice
