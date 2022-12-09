79098d35ab3e91fb9e2bba33b37bb2115d2b6e8b
gjoree <gjorejanevski@yahoo.com>
2022-12-09 16:29:50 +0100

    we are not finished with the back-end

--- CMS/index.js
+++ CMS/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5032
+const novice = require("./routes/novice")
 
-app.listen(port)
 app.get("/",(request, response) => {
 	response.send("Hehe TOP G")
 })
+
+aapp.use('/novice', novice);
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log(`Server is running on port: ${process.env.PORT || port}`)
+    })
