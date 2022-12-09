13c174f7622003db0c0b958a2c1fb4376471efce
Valentino Ivanovski <valentino.ivanovski@icloud.com>
2022-12-09 16:29:47 +0100

    Backend partially finished

--- CMS/index.js
+++ CMS/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+require("dotenv").config()
 const app = express() //framework that makes working with https requests easier
-const port = 5002
+const port = 5052
+const novice = require("./routes/novice")
 
 app.get("/",(request, response)=>{
     response.send("Hello this is Tino.")
 })
+
+app.use("/novice", novice)
 
 app.listen(process.env.PORT || port, ()=>{
     console.log(`Server is running on port: ${process.env.PORT || port}`)
