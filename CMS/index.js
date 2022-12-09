d54671323ec14cae35890bd44b650bd49d93bd70
Anja Pijak <89211024@student.upr.si>
2022-12-09 16:30:00 +0100

    Backend partially finished

--- CMS/index.js
+++ CMS/index.js
@@ -1,12 +1,16 @@
-var express =require('express')
+const express = require("express")
+require("dotenv").config()
 const app = express()
+const port = 5035
+const novice = require("./routes/novice")
 
-const port = 5009
-
-app.get("/", (req,res)=>{
-    res.send("haiiii")
+app.get("/", (req, res)=>{
+    res.send("Hola muchachos...")
 })
 
+app.use("/novice", novice)
+
 app.listen(process.env.PORT || port, ()=>{
-    console.log(`Server is running on port: ${process.env.PORT || port}`)
+    console.log(`Server is running on port: ${process.env.PORT 
+        || port}`)
 })
