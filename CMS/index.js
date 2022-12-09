3be5c144f2c4fe162e0d0299db5e1b96d873806f
todorsergej <petrovictodorsergej@gmail.com>
2022-12-09 16:30:48 +0100

    backend partially finished

--- CMS/index.js
+++ CMS/index.js
@@ -1,11 +1,15 @@
 const express = require("express")
+require("dotenv").config()
 const app = express()
-const port = 5080
+const port = 5049
+const novice = require("./routes/novice")
 
 app.get("/",(req,res)=>{
     res.send("Hola muchachos...")
 })
 
+app.use("/novice", novice)
+
 app.listen(process.env.PORT || port, ()=>{
-    console.log("Server is running on portsa:" + port)
+    console.log("Server is running on port: + ${process.env.PORT || port")
 })
