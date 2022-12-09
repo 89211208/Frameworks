f59cb4f17c3b66fbce8ae182a446213393d46c39
89211025 <89211025@student.upr.si>
2022-12-09 16:30:17 +0100

    Backend partialy finished by mia

--- CMS/index.js
+++ CMS/index.js
@@ -1,11 +1,14 @@
 const express = require("express")
+require("dotenv").config()
 const app = express()
-
-const port = 5011
+const port = 5048
+const novice = require("./routes/novice")
 
 app.get("/",(req, res)=>{
     res.send ("Hi")
 })
+
+app.use("/novice", novice)
 
 //app should listen
 app.listen(process.env.PORT || port, ()=>{
