7337694b2c4dfdad38189f308bf821ae77b01497
vlado <vlado44444@gmail.com>
2022-12-09 22:38:28 +0100

    Commited fiels

--- CMS/index.js
+++ CMS/index.js
@@ -1,12 +1,17 @@
 const express = require("express")
+require("dotenv").config()
 const app = express()
-const port = 5000
+const port = 5017
 
-app.get("/", (req, res)=>{
-    res.send("Hola muchachos...")
+const novice = require("./routes/novice")
+
+app.use("/novice",novice)
+
+app.get("/",(req,res)=>{
+    res.send("Thank you..Boi")
 })
 
+// App should listen 
 app.listen(process.env.PORT || port, ()=>{
-    console.log(`Server is running on port: ${process.env.PORT 
-        || port}`)
+    console.log(`Server is running on port: ${process.env.PORT || port}`)
 })
