d43ea043c07ad14c11336825c2765825659cc3a0
jakobb01 <89211306@student.upr.si>
2022-12-13 18:07:39 +0100

    novice route created!

--- CMS/index.js
+++ CMS/index.js
@@ -2,9 +2,13 @@ const express = require("express")
 const app = express()
 const port = 5009
 
+const novice = require("./routes/novice")
+
 app.get("/", (req, res)=>{
     res.send("Hola muchanchos...")
 })
+
+app.use("/novice", novice)
 
 app.listen(process.env.PORT || port, ()=>{
     console.log(`Server is running on port: ${process.env.PORT || port}`)
