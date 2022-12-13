ee963e914076677f25a93fcb5cbc05eb80589559
Maja Kovac <89191078@student.upr.si>
2022-12-13 18:09:38 +0100

    added routes

--- CMS/index.js
+++ CMS/index.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const app = express()
-const port = 5000
+const port = 5061
+
+const novice= require("./routes/novice")
+
 
 app.get("/", (req, res)=>{
     res.send("Hola muchachos...")
 })
+
+app.use('/novice', novice);
 
 app.listen(process.env.PORT || port, ()=>{
     console.log(`Server is running on port: ${process.env.PORT 
