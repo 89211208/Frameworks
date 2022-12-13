f9d814028f8ac233c852e305d9038a815ca5fb8c
Tiggax <66419530+Tiggax@users.noreply.github.com>
2022-12-13 18:12:36 +0100

    added novice webpage

--- CMS/index.js
+++ CMS/index.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const app = express()
-const port = 5000
+const port = 5058
 
+
+//Import opur custom modules-controllers
+const novice= require("./routes/novice")
+//Routes
 app.get("/", (req, res)=>{
     res.send("Hola muchachos...")
 })
+
+app.use('/novice', novice);
 
 app.listen(process.env.PORT || port, ()=>{
     console.log(`Server is running on port: ${process.env.PORT 
