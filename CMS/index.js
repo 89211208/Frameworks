ebec4bb7e055655294d53a12656b0cd1b17cafc0
EvaGK <116080925+EvaGK@users.noreply.github.com>
2022-12-13 18:58:02 +0100

    Back end ready without users

--- CMS/index.js
+++ CMS/index.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const app = express()
-const port = 5000
+require("dotenv").config()
+const port = 5059
+
+const novice = require("./routes/novice")
+const users = require("./routes/users")
 
 app.get("/", (req, res)=>{
     res.send("Hola muchachos...")
 })
+
+app.use('/novice', novice);
 
 app.listen(process.env.PORT || port, ()=>{
     console.log(`Server is running on port: ${process.env.PORT 
