55bc9dd4bbb1ac94cdcd8977a80a348dbba46e4a
Amar <amar.ustavdija@gmail.com>
2022-12-13 18:07:51 +0100

    novice changes

--- CMS/index.js
+++ CMS/index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
-const port = 1337
+const port = 5077
+const novice = require("./routes/novice")
 
 app.get("/", (req, res)=>{
 	res.send("Hola, opaaaalaaaa")
 })
+
+app.use("/novice", novice)
 
 app.listen(process.env.PORT || port, ()=>{
 	console.log(`Server is running on port: ${process.env.PORT || port}`)
---
+++ CMS/routes/novice.js
@@ -0,0 +1,9 @@
+const express = requrie("express")
+const novice = express.Router()
+
+novice.get("/", (req,res)=>{
+    console.log("The route has been reached!")
+    res.send("novice")
+})
+
+module.exports = novice
