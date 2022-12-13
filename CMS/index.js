4d9616982e2042e5ecaa8db759d27e0610aa8f3e
Tiggax <66419530+Tiggax@users.noreply.github.com>
2022-12-13 19:46:34 +0100

    all from lab work

--- CMS/index.js
+++ CMS/index.js
@@ -1,18 +1,21 @@
-const express = require("express")
+const express = require('express')
+require('dotenv').config()
+
 const app = express()
 const port = 5058
-
 
 //Import opur custom modules-controllers
 const novice= require("./routes/novice")
 //Routes
-app.get("/", (req, res)=>{
-    res.send("Hola muchachos...")
-})
-
 app.use('/novice', novice);
 
-app.listen(process.env.PORT || port, ()=>{
-    console.log(`Server is running on port: ${process.env.PORT 
-        || port}`)
+app.get("/",(req,res)=>{
+res.send("hola")
 })
+
+///App listening on port
+app.listen(process.env.PORT || port, ()=>{
+console.log(`Server is running on port: ${process.env.PORT || port}`)
+})
+
+
