6c110db0306c91147f6dd246df8b148831023151
Amar <amar.ustavdija@gmail.com>
2022-12-16 17:04:37 +0100

    update

---
+++ CMS/DB/dbConn.js
@@ -0,0 +1,47 @@
+const mysql = require("mysql2")
+const conn = mysql.createConnection({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:"Qcodeigniter"
+})
+
+let dataPool = {}
+
+dataPool.allNovice = () =>{
+    return new Promise((resolve, reject)=>{
+        conn.query(`SELECT * FROM news`, (err, res)=>{
+            if(err){return reject(err)}
+            return resolve(res)
+        })
+    })
+}
+
+dataPool.oneNovice = (id)=>{
+    return new Promise((resolve, reject)=>{
+        conn.query(`SELECT * FROM news WHERE id = ?`, id, (err,res)=>{
+            if(err){return reject(err)}
+            return resolve(res)
+        })
+    })
+}
+
+dataPool.createNovica = ()=>{
+    return new Promise((resolve, reject)=>{
+        conn.query(`INSERT INTO news (title, slug, text) VALUES (?,?,?)`, [title,slug,text], (err,res)=>{
+            if(err){return reject(err)}
+            return resolve(res)
+        })
+    })
+}
+
+
+conn.connect((err)=>{
+    if (err) {
+        console.log("ERROR: " + err.message)
+        return
+    }
+    console.log("Connection established")
+})
+
+module.exports = dataPool
--- CMS/routes/novice.js
+++ CMS/routes/novice.js
@@ -1,9 +1,20 @@
 const express = require("express")
 const novice = express.Router()
+const DB = require("../DB/dbConn")
 
 novice.get("/", (req,res)=>{
     console.log("The route has been reached!")
     res.send("novice")
+})
+
+novice.get("/", async (req,res, next)=>{
+    try{
+        let queryResult = await DB.allNovice()
+        res.json(queryResult)
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
 })
 
 module.exports = novice
