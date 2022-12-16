6bc4c125cd9bea48d26f85d40c6dab63494981f1
svenahac <ahacsh@gmail.com>
2022-12-16 20:24:16 +0100

    completed frontend tutorial

---
+++ CMS/client-dev/src/CustomComponents/LoginView.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+class LoginView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        type: "signup",
+      },
+    };
+  }
+  QGetTextFromField = (e) => {
+    this.setState((prevState) => ({
+      user: { ...prevState.user, [e.target.name]: e.target.value },
+    }));
+  };
+
+  QSentUserToParent = () => {
+    this.props.QUserFromChild(this.state.user);
+  };
+
+  render() {
+    return (
+      <div
+        className="card"
+        style={{
+          width: "400px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: "10px",
+          marginBottom: "10px",
+        }}
+      >
+        <form style={{ margin: "20px" }}>
+          <div className="mb-3">
+            <label className="form-label">Username</label>
+            <input
+              onChange={(e) => this.QGetTextFromField(e)}
+              name="username"
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input
+              onChange={(e) => this.QGetTextFromField(e)}
+              name="password"
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+        </form>
+        <button
+          onClick={() => this.QSentUserToParent()}
+          style={{ margin: "10px" }}
+          className="btn btn-primary bt"
+        >
+          Login
+        </button>
+      </div>
+    );
+  }
+}
+
+export default LoginView;
