0f94a6776200d1e3fe02dff4212fa58819c4b69b
Amar <amar.ustavdija@gmail.com>
2022-12-16 18:34:33 +0100

    const users

--- CMS/index.js
+++ CMS/index.js
@@ -3,12 +3,15 @@ const app = express()
 require("dotenv").config()
 const port = 1337
 const novice = require("./routes/novice")
+const users = require("./routes/users")
 
 app.get("/", (req, res)=>{
 	res.send("Hola, opaaaalaaaa")
 })
 
 app.use("/novice", novice)
+
+app.use("/users", users)
 
 app.listen(process.env.PORT || port, ()=>{
 	console.log(`Server is running on port: ${process.env.PORT || port}`)
