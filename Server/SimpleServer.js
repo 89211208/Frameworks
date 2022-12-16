62000cb6e22a94db2c07e3a57bb706b840a3256f
89211187 <89211187@student.upr.si>
2022-12-16 12:29:17 +0100

    server and routes done

---
+++ Server/SimpleServer.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-Type": "text/html"})
+    res.write("\nUser is in: " + req.url)
+    res.write("<h1>This line has been instantiated from the server...</h1>")
+    res.end()
+    }).listen(8000)
+    
