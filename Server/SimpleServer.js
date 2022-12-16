9155db70b55814254ec5b47cea6f7150340780c6
Valeryia Bazhko <89211036@student.upr.si>
2022-12-16 21:31:09 +0100

    front-end// backend doesn't work

--- Server/SimpleServer.js
+++ Server/SimpleServer.js
@@ -4,4 +4,4 @@ http.createServer((req, res)=>{
     res.write("<h1>This line has been instantiated from the server</h1>")
     res.write("\n User is in: " + req.url )
     res.end()
-}).listen(8055)
+}).listen(5004)
