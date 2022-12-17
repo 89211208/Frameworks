d34717958b59316265bc4a0d03b1ea6049123fdd
UNovak <urbanwritescode@gmail.com>
2022-12-17 17:38:28 +0100

    Changes made following the first part of front-end tutorial

---
+++ client-dev/src/App.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+//import Custom Components
+import AboutView from "./CustomComponents/AboutView.js";
+import AddNovicaView from "./CustomComponents/AddNovicaView.js";
+import HomeView from "./CustomComponents/HomeView.js";
+import LoginView from "./CustomComponents/LoginView.js";
+import NoviceView from "./CustomComponents/NoviceView.js";
+import SignupView from "./CustomComponents/SignupView.js";
+import SingleNovicaView from "./CustomComponents/SingleNovicaView.js";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="APP" className="container">
+        <div id="menu" className="row">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="#">
+                Home
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link " href="#">
+                      About
+                    </a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a className="nav-link " href="#">
+                      News
+                    </a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a className="nav-link">Add news</a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a className="nav-link " href="#">
+                      Sign up
+                    </a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a className="nav-link " href="#">
+                      Login
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+
+        <div id="viewer" className="row container">
+          <HomeView/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
