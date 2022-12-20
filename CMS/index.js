2888d1323489b4cccfa7c589f04c27ff7a07405c
Michael_Silver <michael2silver@gmail.com>
2022-12-20 14:20:23 +0100

    completed backend

--- CMS/index.js
+++ CMS/index.js
@@ -1,19 +1,17 @@
-const express = require("express")
-const app = express()
-require("dotenv").config()
-const port = 5000
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = 5070;
 
-const novice = require("./routes/novice")
-const users = require("./routes/users")
+const novice = require("./routes/novice");
+const users = require("./routes/users");
 
-app.get("/", (req, res)=>{
-    res.send("Hola muchachos...")
-})
+app.get("/", (req, res) => {
+  res.send("Hola muchachos...");
+});
 
-app.use("/novice", novice)
+app.use("/novice", novice);
 
-
-app.listen(process.env.PORT || port, ()=>{
-    console.log(`Server is running on port: ${process.env.PORT 
-        || port}`)
-})
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server is running on port: ${process.env.PORT || port}`);
+});
