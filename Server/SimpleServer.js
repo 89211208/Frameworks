098b30f856a430c918a6d20d47a828141fb985c3
89211177 <89211177@student.upr.si>
2022-12-20 17:28:11 +0100

    BackEnd Complete

--- Server/SimpleServer.js
+++ Server/SimpleServer.js
@@ -1,7 +1,7 @@
 var http = require("http")
 
 http.createServer((req, res)=>{
-    res.write("<h1>This line has been instantiated from the server</h1>")
+    res.write("<h1>This line has been instantiated from the server...</h1>")
     res.write("\n User is in: " + req.url )
     res.end()
-}).listen(8055)
+}).listen(8011)
