c1a036acb4bb5de709bb92f8c0aa59b72c563ba4
UNovak <urbanwritescode@gmail.com>
2022-12-23 17:01:40 +0100

    tried merging with axios but needs a fix

--- client-dev/src/CustomComponents/NoviceView.js
+++ client-dev/src/CustomComponents/NoviceView.js
@@ -1,32 +1,59 @@
 import React from "react";
+import axios from 'axios';
 
 class NoviceView extends React.Component {
+
+
   QSetViewInParent = (obj) => {
     this.props.QIDFromChild(obj);
   };
 
+  constructor(props)
+  {
+    super(props);
+    this.state={
+        novice:[]
+    }
+  }
+
+  componentDidMount()
+  {
+    axios.get('http://88.200.63.148:5014/novice')
+    .then(response=>{
+        console.log(response.data)
+      this.setState({
+        novice:response.data
+      })
+    })
+  }
+  
+
+
+
+
   render() {
+
+    let data=this.state.data
+
     return (
-      <div
-        className="row row-cols-1 row-cols-md-3 g-4"
-        style={{ margin: "10px" }}
-      >
-        <div className="col">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">Slug</p>
-            </div>
-            <button
-              onClick={() => this.QSetViewInParent({ page: "novica" })}
-              style={{ margin: "10px" }}
-              className="btn btn-primary bt"
-            >
-              Read more
-            </button>
+      <div className="row row-cols-1 row-cols-md-3 g-4" style={{margin:"10px"}}>
+      {data.length > 0 ?
+          data.map((d)=>{
+              return(
+                  <div className="col" key={d.id}>
+                  <div className="card">
+                      <div className="card-body">
+                          <h5 className="card-title">{d.title}</h5>
+                          <p className="card-text">{d.slug}</p>
+                      </div>
+                      <button style={{margin:"10px"}}  className="btn btn-primary bt">Read more</button>
+                  </div>
+                  </div>
+                  )
+              })
+               :"Loading..."}
           </div>
-        </div>
-      </div>
+  
     );
   }
 }
