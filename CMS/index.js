40f7d3308906c6a8541d5665d7bd12537a37c5fd
89201013 <89201013@example.com>
2022-12-23 15:40:04 +0100

    Reset again cuz im dumb XD

--- CMS/index.js
+++ CMS/index.js
@@ -1,30 +1,19 @@
-
-const express = require('express')
+const express = require("express")
 const app = express()
 require("dotenv").config()
+const port = 5000
 
-const port = 5052
-
-//Configs
-app.use(express.json())
-
-//Import opur custom modules-controllers
 const novice = require("./routes/novice")
-
-
-
-const conn = require("./DB/dbConn")
 const users = require("./routes/users")
-app.get('/', (req, res)=>{
-    res.send("hello")
-    res.end()
+
+app.get("/", (req, res)=>{
+    res.send("Hola muchachos...")
 })
 
-//Routes
-app.use('/novice', novice);
-app.use('/users', users)
+app.use("/novice", novice)
+
 
 app.listen(process.env.PORT || port, ()=>{
-    console.log(`Server is running on port: ${process.env.PORT || port}`)
+    console.log(`Server is running on port: ${process.env.PORT 
+        || port}`)
 })
-
