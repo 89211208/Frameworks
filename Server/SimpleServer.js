47c3e3d86d6052177a7b65266e7e1d150303450b
Q-AU <cc.mijangos@outlook.com>
2022-12-30 14:42:18 +0100

    Cleaned dir

--- Server/SimpleServer.js
+++
@@ -1,7 +0,0 @@
-var http = require("http")
-
-http.createServer((req, res)=>{
-    res.write("<h1>This line has been instantiated from the server</h1>")
-    res.write("\n User is in: " + req.url )
-    res.end()
-}).listen(8055)
