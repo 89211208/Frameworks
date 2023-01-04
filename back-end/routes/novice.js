4b47f0dc0dff7a3c8f1b0e5c4c28b554593489a6
Veljko Spasic <89211208@student.upr.si>
2023-01-04 16:00:52 +0100

    CRUD part 2 completed

--- back-end/routes/novice.js
+++ back-end/routes/novice.js
@@ -1,9 +1,47 @@
 const express= require("express")
 const novice = express.Router();
+const db = require("../db/conn")
 
-novice.get('/',(req,res)=>{
-    console.log("The route has been reached")
-    res.json({title:"Hola"})
+novice.get('/', async (req, res, next)=>{
+    try {
+        let queryResult = await db.allNovice()
+        res.json(queryResult)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+
+novice.get("/:id", async (req, res)=>{
+    try {
+        console.log(req)
+        let queryResult = await db.oneNovica(req.params.id)
+        res.json(queryResult)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+
+novice.post("/", async (req, res)=>{
+    let [title, slug, text] = req.body
+    let isAcompleteNovica = title && slug && text
+
+    if (isAcompleteNovica ) {
+        try {
+            let queryResult = await db.createNovica(title, slug, text)
+            if (queryResult.affectedRows) {
+                console.log("New article added!")
+            }
+        } catch (error) {
+            console.log(error)
+            res.sendStatus(500)
+        }
+    }else {
+        console.log("A field is empty...")
+    }
+    res.end()
+    
 })
 
 module.exports=novice
---
+++ back-end/routes/user.js
@@ -0,0 +1,47 @@
+const express= require("express")
+const users = express.Router();
+const db = require("../db/conn")
+
+users.post("/login", async(req, res)=>{
+    let [username, password] = req.body
+    let isUserComplete = username && password
+    if (isUserComplete) {
+        try {
+            let queryResult = await db.authUser(username)
+            if (queryResult.length > 0) {
+                if (password === queryResult[0].user_password){
+                    console.log(queryResult)
+                }else{
+                    console.log("INCORRECT PASSWORD")
+                }
+            }else{
+                console.log("USER NOT REGISTERED")
+            }
+        } catch (error) {
+            console.log(error)
+            res.sendStatus(500)
+        }
+    }else{
+        console.log("Please enter username and password.")
+    }
+    res.end()
+})
+
+users.post("/register",async(req, res)=>{
+    let [username, password, email] = req.body
+    let isUserComplete = username && password && email
+    if (isUserComplete){
+        try {
+            let queryResult = await db.addUser(username, email, password)
+            if (queryResult.affectedRows){
+                console.log("New user added!")
+            }
+        } catch (error) {
+            console.log(error)
+            res.sendStatus(500)
+        }
+    }else{
+        console.log("A field is missing...")
+    }
+    res.end()
+})
