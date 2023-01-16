54be9b74b328454ae033ba8baad43797c35dde4d
Veljko Spasic <89211208@student.upr.si>
2023-01-16 17:31:14 +0100

    Some css added to improve appereance

--- front-end/src/CustomComponents/HomeView.js
+++ front-end/src/CustomComponents/HomeView.js
@@ -6,7 +6,7 @@ class HomeView extends Component {
       <div className="card" style={{ margin: "10px" }}>
         <div className="card-body">
           <h5 className="card-title">Welcome!!!</h5>
-          <p className="card-text">You are in hte home page</p>
+          <p className="card-text">You are in the home page</p>
         </div>
       </div>
     );
